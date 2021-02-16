61f51387cd75c038b1e930e380ef47018bb85d54
Raihan Hafiizh <raihanhafis212@gmail.com>
2021-02-16 14:11:28 +0700

    Operator Logika

---
+++ 15-Operator Logika/src/com/tutorial/Main.java
@@ -0,0 +1,66 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Operator Logika OR, AND, XOR
+        boolean a, b, c;
+
+        //OR
+        System.out.println("==== OR (||) ====");
+        a = false;
+        b = false;
+        c = (a || b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a || b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a || b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a || b);
+        System.out.println(a + " || " + b + " = " + c);
+
+        //AND
+        System.out.println("==== AND (&&) ====");
+        a = false;
+        b = false;
+        c = (a && b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a && b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a && b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a && b);
+        System.out.println(a + " && " + b + " = " + c);
+
+        //XOR (exclusive or)
+        System.out.println("==== AND (^) ====");
+        a = false;
+        b = false;
+        c = (a ^ b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a ^ b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a ^ b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a ^ b);
+        System.out.println(a + " ^ " + b + " = " + c);
+    }
+}
