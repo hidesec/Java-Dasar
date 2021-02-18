6a49f5a43652a5a9c940b6afed9f131fef94eb6f
Raihan Hafiizh <raihanhafis212@gmail.com>
2021-02-18 19:49:43 +0700

    IO

---
+++ 62-IO Stream/src/com/tutorial/Main.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInput = new FileInputStream("input.txt");
+
+        System.out.println(fileInput.read());
+    }
+}
